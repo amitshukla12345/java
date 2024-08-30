ae6e71151407b71a9a73982a5e94415cd885ef52
amitshukla12345 <amitshukla22509@gmail.com>
2024-08-30 09:07:26 +0530

    first commit

---
+++ bit.java
@@ -0,0 +1,26 @@
+class bit
+{
+	public static void main(String arg[])
+	{
+		/*Bitwise operators
+		Bitwise and & If all bits are 1 then it gives one otherwise 0
+		Bitwise or | if either of bit is one then you will get one if all
+		bits are zero then you will get zero
+		
+		Bitwise XoR ^
+		if bit are different then 1 and if bits are some then zero
+		Bitwise not ~ if bit is one then you will get 0 and if if is 1  then you will get zero
+		compiler will return 2's complement of a number
+		*/
+		System.out.println(15&6);
+		// or 
+		System.out.println(15|6);
+		// xor
+		System.out.println(15^6);
+		// not ~
+		System.out.println(~15);
+		
+	
+		
+	}
+}
---
+++ ternary.java
@@ -0,0 +1,9 @@
+class ternary
+{
+	public static void main(String args[])
+	{
+		int a=67;
+		int b=30;
+		System.out.println(a>b?"a is greater":"b is greater");
+	}
+}

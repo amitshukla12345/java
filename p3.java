b57fe82e7a244bbe3f27785ff8ced867b3702495
amitshukla12345 <amitshukla22509@gmail.com>
2024-09-02 08:59:41 +0530

    OBJECT

---
+++ p3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class p3
+{
+	public static void main(String args[])
+	{
+		/*String name="Hello";
+		System.out.println("My name is "+name);*/
+		
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		System.out.println("Value given by user is"+a);
+		
+		
+	}
+}
---
+++ p5.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class p5
+{
+	public static void main(String args[])
+	{
+		/* Scanner sc=new Scanner(System.in);
+		// next nextLine()
+		System.out.println("Enter first name");
+		String firstName=sc.next();
+		System.out.println("My name is"+firstName); */
+		
+		/*Scanner sc=new Scanner(System.in);
+		String sentence=sc.nextLine();
+		System.out.println(sentence);*/
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter your name");
+		String firstName=sc.nextLine();
+		
+		System.out.println("Enter your age");
+		int a=sc.nextInt();
+		System.out.println("My name is amit shukla and my age is 21");
+		
+		 
+		
+	}
+}

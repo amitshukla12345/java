6920bdec0227eee30436f676c2c566a35b577fe7
amitshukla12345 <amitshukla22509@gmail.com>
2024-09-06 13:33:57 +0530

    first commit

---
+++ p15.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class p15
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		switch(n)
+		{
+			case 1:
+			System.out.println("Monday");
+			break;
+			case 2:
+			System.out.println("Tuesday");
+			break;
+			case 3:
+			System.out.println("Wednesday");
+			break;
+			case 4:
+			System.out.println("Thursday");
+			break;
+			case 5:
+			System.out.println("Thursday");
+			break;
+			case 6:
+			break;
+			default:
+			System.out.println("no matching case");
+			
+			
+		}
+	}
+}

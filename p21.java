5f0b9d762dc7d000c2cfe6d7e2c88221ef959207
amitshukla12345 <amitshukla22509@gmail.com>
2024-09-10 08:53:56 +0530

    first commit

---
+++ p21.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class p21
+{
+	public static void main(String args[])
+	{
+		int sum=0;
+		for(int i=1; i<=5;i++)
+		{
+			sum=sum+i;
+		}
+		System.out.println("sum is "+sum);
+		
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		
+		int n=3;
+		int fact=i;
+		for(int i=1; i<=n; i++)
+		{
+			fact=fact*i;
+		}
+		System.out.println(fact);
+	}
+}
